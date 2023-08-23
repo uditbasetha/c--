98383cc9e6f010091cf3a2c7a1a9df1545f06d80
udit basetha <uditbasetha2004@gmail.com>
2023-08-23 12:30:21 +0530

    first commit

---
+++ first.cpp/1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a;
+    cin >> a;
+    cout << "value of a is =" << a << endl;
+
+    int b;
+    cin >> b;
+    cout << "va;lue of b is =" << b << endl;
+
+    cout << " addition of a and b=" << a + b << endl;
+    cout << sizeof(a) << endl;
+    float f = 1.5;
+    cout << sizeof(f) << endl;
+
+    double d = 3.5;
+
+    int x = (int)d + 2;
+    cout << "value of x is=" << x << endl;
+}
---
+++ first.cpp/3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // int score;
+    // cin >> score;
+    // cout << "enter score=" << score << endl;
+
+    // if (score > 300)
+    // {
+    //     cout << "indiam win" << endl;
+    // }
+    // else
+    // {
+    //     cout << "pak win" << endl;
+    // }
+
+    // new code
+
+    int age;
+    cin >> age;
+    cout << "enter your age=" << endl;
+
+    if (age < 18)
+    {
+        cout << "you are not eligiable for vote" << endl;
+    }
+    else
+    {
+        cout << "you are eligiable for voting" << endl;
+    }
+}
